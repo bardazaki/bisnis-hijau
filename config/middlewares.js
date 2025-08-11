43a517bbbc7dcad9e2c40de0226d8c987caf5d35
bardazaki <muhamadbardazaki@gmail.com>
2025-08-11 12:52:13 +0700

    allow Cloudinary in CSP

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,25 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+  module.exports = [
+    'strapi::logger',
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:', 'http:', 'ws:', 'wss:', 'res.cloudinary.com'],
+            'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+            'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+            'frame-src': ["'self'", 'blob:', 'res.cloudinary.com'],
+          },
+        },
+      },
+    },    
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
